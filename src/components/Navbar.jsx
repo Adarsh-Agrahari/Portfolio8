4f7d5595306a005c6d044b93bbfdb3e91e642274
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-11-26 22:21:08 +0530

    Update navbar and footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import styles from "@/styles/Navbar.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { IoIosArrowDown } from "react-icons/io";
 import { RxCross1 } from "react-icons/rx";
 import Link from "next/link";
 
 export default function Navbar() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
-	const menuRef = useRef(null);
 
-	const handleHamburgerClick = (event) => {
+	const toggleMenu = () => {
 		setIsMenuOpen(!isMenuOpen);
 	};
 
@@ -18,33 +16,25 @@ export default function Navbar() {
 			<div className={styles.logo}>
 				<Link href="/">SKP</Link>
 			</div>
-			<div className={styles.navLinks}>
-				<Link href="/"> Home </Link>
-				<Link href="/articles"> Articles </Link>
-				<Link href="/about"> About </Link>
-				<Link href="/"> Award </Link>
-				<Link href="/books"> Books Authored </Link>
-				<Link href="/news"> News </Link>
+			<div className={`${styles.navLinks} ${isMenuOpen ? styles.hideLinks : ""}`}>
+				<Link href="/">Home</Link>
+				<Link href="/articles">Articles</Link>
+				<Link href="/about">About</Link>
+				<Link href="/">Award</Link>
+				<Link href="/books">Books Authored</Link>
+				<Link href="/news">News</Link>
 			</div>
-			{isMenuOpen ? (
-				<RxCross1
-					className={styles.menuIcon}
-					onClick={handleHamburgerClick}
-				/>
-			) : (
-				<GiHamburgerMenu
-					className={styles.menuIcon}
-					onClick={handleHamburgerClick}
-				/>
-			)}
+			<div className={styles.menuIcon} onClick={toggleMenu}>
+				{isMenuOpen ? <RxCross1 /> : <GiHamburgerMenu />}
+			</div>
 			{isMenuOpen && (
-				<div ref={menuRef} className={styles.menu}>
-					<Link href="/"> Home </Link>
-					<Link href="/articles"> Articles </Link>
-					<Link href="/about"> About </Link>
-					<Link href="/"> Award </Link>
-					<Link href="/books"> Books Authored </Link>
-					<Link href="/news"> News </Link>
+				<div className={styles.mobileMenu}>
+					<Link href="/" onClick={toggleMenu}>Home</Link>
+					<Link href="/articles" onClick={toggleMenu}>Articles</Link>
+					<Link href="/about" onClick={toggleMenu}>About</Link>
+					<Link href="/" onClick={toggleMenu}>Award</Link>
+					<Link href="/books" onClick={toggleMenu}>Books Authored</Link>
+					<Link href="/news" onClick={toggleMenu}>News</Link>
 				</div>
 			)}
 		</nav>
